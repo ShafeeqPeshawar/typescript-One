f8f0cf95f6a1a8d7cefd3259581f2dfa026577f8
Shafeeqpeshawar <Shafeeq.peshawar@gmail.com>
2025-03-16 00:07:25 +0000

    7th Commit

---
+++ Projects/src/2-Advance/53-A-Unknown-Type.js
@@ -0,0 +1,8 @@
+
+/*
+
+unknown Type in TypeScript
+The unknown type is a safer alternative to - any- because it forces type checking before usage. 
+It represents a value that could be anything, but unlike any, you must perform type checks before using it.
+
+*/
---
+++ Projects/src/6-Generics/1-0-Generics.ts
@@ -0,0 +1,9 @@
+/*
+
+Generics in TypeScript
+
+Generics allow you to create reusable, type-safe components, functions, 
+and classes that work with different types without losing type information. 
+They help in writing flexible yet strongly typed code.
+
+*/
